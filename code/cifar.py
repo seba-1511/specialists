71f0ea3bc5287f67dc292c7dbd3f9b2a42d199dd
seba-1511 <seba-1511@hotmail.com>
2015-05-16 23:50:55 -0700

    Added dataset files

---
+++ code/cifar.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+logging.basicConfig(level=20)
+logger = logging.getLogger()
+
+from neon.backends import gen_backend
+from neon.layers import FCLayer, DataLayer, CostLayer
+from neon.models import MLP
+from neon.transforms import RectLin, Logistic, CrossEntropy
+from neon.datasets import CIFAR100
+from neon.experiments import FitPredictErrorExperiment
+
+
+def create_model(nin):
+    layers = []
+    layers.append(DataLayer(nout=nin))
+    layers.append(FCLayer(nout=100, activation=RectLin()))
+    layers.append(FCLayer(nout=10, activation=Logistic()))
+    layers.append(CostLayer(cost=CrossEntropy()))
+    model = MLP(num_epochs=10, batch_size=128, layers=layers)
+    return model
+
+
+def run():
+    model = create_model(nin=3072)
+    backend = gen_backend(rng_seed=0)
+    dataset = CIFAR100(repo_path='~/data/')
+    import pdb; pdb.set_trace()
+    experiment = FitPredictErrorExperiment(model=model,
+                                           backend=backend,
+                                           dataset=dataset)
+    experiment.run()
+
+
+if __name__ == '__main__':
+    run()
