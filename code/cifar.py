d8177c03372296c88dc1017a76884f82140f6c09
seba-1511 <seba-1511@hotmail.com>
2015-05-17 17:51:30 -0700

    Added basic code for running cifar example.

--- code/cifar.py
+++ code/cifar.py
@@ -7,7 +7,13 @@ logging.basicConfig(level=20)
 logger = logging.getLogger()
 
 from neon.backends import gen_backend
-from neon.layers import FCLayer, DataLayer, CostLayer
+from neon.layers import (
+    FCLayer,
+    DataLayer,
+    CostLayer,
+    ConvLayer,
+    PoolingLayer,
+)
 from neon.models import MLP
 from neon.transforms import RectLin, Logistic, CrossEntropy
 from neon.datasets import CIFAR100
@@ -15,11 +21,16 @@ from neon.experiments import FitPredictErrorExperiment
 
 
 def create_model(nin):
-    layers = []
-    layers.append(DataLayer(nout=nin))
-    layers.append(FCLayer(nout=100, activation=RectLin()))
-    layers.append(FCLayer(nout=10, activation=Logistic()))
-    layers.append(CostLayer(cost=CrossEntropy()))
+    layers = [
+        DataLayer(nofm=3, ofmshape=[32, 32], is_local=True),
+        ConvLayer(nofm=16, fshape=[5, 5]),
+        PoolingLayer(op='max', fshape=[2, 2], stride=2),
+        ConvLayer(nofm=32, fshape=[5, 5]),
+        PoolingLayer(op='max', fshape=[2, 2], stride=2),
+        FCLayer(nout=500, activation=RectLin()),
+        FCLayer(nout=100, activation=Logistic()),
+        CostLayer(cost=CrossEntropy()),
+    ]
     model = MLP(num_epochs=10, batch_size=128, layers=layers)
     return model
 
@@ -28,7 +39,6 @@ def run():
     model = create_model(nin=3072)
     backend = gen_backend(rng_seed=0)
     dataset = CIFAR100(repo_path='~/data/')
-    import pdb; pdb.set_trace()
     experiment = FitPredictErrorExperiment(model=model,
                                            backend=backend,
                                            dataset=dataset)
