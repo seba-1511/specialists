66d3bc3d12aa92856f57761f9006a221826d98fb
seba-1511 <seba-1511@hotmail.com>
2015-05-18 19:59:01 +0000

    increased network size.

--- code/cifar.py
+++ code/cifar.py
@@ -15,9 +15,10 @@ from neon.layers import (
     PoolingLayer,
 )
 from neon.models import MLP
-from neon.transforms import RectLin, Logistic, CrossEntropy
+from neon.transforms import RectLin, Logistic, CrossEntropy, Softmax
 from neon.datasets import CIFAR100
 from neon.experiments import FitPredictErrorExperiment
+from neon.optimizers import GradientDescentMomentum
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -36,31 +37,49 @@ def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
 def create_model(nin):
     layers = [
         DataLayer(nofm=3, ofmshape=[32, 32], is_local=True),
-        #ConvLayer(nofm=16, fshape=[5, 5]),
-        #PoolingLayer(op='max', fshape=[2, 2], stride=2),
-        #ConvLayer(nofm=32, fshape=[5, 5]),
-        #PoolingLayer(op='max', fshape=[2, 2], stride=2),
-        FCLayer(nin=32*32*3, nout=500, activation=RectLin()),
-        FCLayer(nin=500, nout=100, activation=Logistic()),
+        ConvLayer(nofm=64, fshape=[5, 5], activation=RectLin()),
+        PoolingLayer(op='max', fshape=[2, 2], stride=1),
+        ConvLayer(nofm=128, fshape=[5, 5], activation=RectLin()),
+        PoolingLayer(op='max', fshape=[2, 2], stride=1),
+        FCLayer(nout=2000, activation=RectLin()),
+        FCLayer(nout=2000, activation=RectLin()),
+        FCLayer(nout=100, activation=Logistic()),
         CostLayer(cost=CrossEntropy()),
     ]
-    model = MLP(num_epochs=3, batch_size=128, layers=layers)
+    model = MLP(num_epochs=300, batch_size=128, layers=layers)
     return model
 
 
 def run():
     model = create_model(nin=3072)
-    backend = gen_backend(rng_seed=0)
+    backend = gen_backend(rng_seed=0, gpu='cudanet')
     dataset = CIFAR100(repo_path='~/data/')
-    model.initialize(backend)
-    model.fit(dataset)
-    pred = model.predict_generator(dataset, 'test')
-    confusion = confusion_matrix(dataset.targets['test'], pred)
-    plot_confusion_matrix(cm=confusion)
-    # experiment = FitPredictErrorExperiment(model=model,
-                                           # backend=backend,
-                                           # dataset=dataset)
-    # experiment.run()
+    momentum_params={
+        'type': 'constant',
+        'coef': 0.8,
+    }
+    lr_params = {
+        'learning_rate': .01,
+        'momentum_params': momentum_params,
+    }
+    lrule = GradientDescentMomentum(lr_params=lr_params, name='gdm')
+    #model.initialize(backend)
+    #model.fit(dataset)
+    #pred = model.predict_generator(dataset, 'test')
+    #confusion = confusion_matrix(dataset.targets['test'], pred)
+    #plot_confusion_matrix(cm=confusion)
+    experiment = FitPredictErrorExperiment(
+        model=model,
+        backend=backend,
+        dataset=dataset,
+        lrule=lrule,
+    )
+    #pred = model.predict_generator(dataset, 'test')
+    #import pdb; pdb.set_trace()
+    #confusion = confusion_matrix(dataset.targets['test'], pred)
+    #plot_confusion_matrix(cm=confusion)
+
+    experiment.run()
 
 
 if __name__ == '__main__':
