4a9a1bccf11694f1108fbb2fc0756e635dc35b03
seba-1511 <seba-1511@hotmail.com>
2015-06-05 13:27:48 -0700

    Implemented greedy clustering, overlapping and not.

--- code/6_gen_spec_data.py
+++ code/6_gen_spec_data.py
@@ -81,7 +81,7 @@ def soft_sum_cm(targets, preds):
         Return a confusion matrix, containing the sum of all probabilities for
         a given class.
    """
-    N = max(targets)
+    N = max(targets) + 1
     nb_preds = len(preds)
     placeholder = np.zeros(nb_preds)
     cm = np.zeros((N, N))
@@ -99,7 +99,7 @@ def soft_sum_pred_cm(targets, preds):
         Returns a confusion matrix, where the the content of a cell is the sum
         of prediction probabilities, only when this class was predicted.
     """
-    N = max(targets)
+    N = max(targets) + 1
     nb_preds = len(preds)
     placeholder = np.zeros(nb_preds)
     cm = np.zeros((N, N))
@@ -123,6 +123,91 @@ def soft_sum_n_pred_cm(targets, preds, n=5):
     pass
 
 
+def unfriendliness_matrix(cm):
+    """
+        Returns a len(cm)**2 matrix friends, where an entry is the friendliness
+        between two classes, based on a confusion matrix.
+        (friends[i, j] = cm[i, j] + cm[j, i])
+    """
+    N = len(cm)
+    return np.array(
+        [[cm[i, j] + cm[j, i] for j in xrange(N)] for i in xrange(N)])
+
+
+def set_score(friends_mat, cls, friends_set):
+    """
+        Returns the friends score for cls, with respect to a given friend set.
+    """
+    return sum([friends_mat[cls, f] for f in friends_set])
+
+
+def greedy_clustering(friends, N):
+    """
+        Returns a list of N sets, so that each set is the most friendly as
+        possible. Note that this does not allow for a class to be in two sets.
+    """
+    nb_classes = len(friends)
+    clusters = [set() for _ in xrange(N)]
+    seen = set()
+    scores = friends.copy()
+    while len(seen) < nb_classes:
+        best = np.argmax(scores)
+        class1, class2 = best = (best / nb_classes, best % nb_classes)
+        if class1 in seen and class2 in seen:
+            scores[class1, class2] = -1
+            scores[class2, class1] = -1
+            continue
+
+        elif class1 in seen:
+            opt_set = [i for i, s in enumerate(clusters) if class1 in s][0]
+        elif class2 in seen:
+            opt_set = [i for i, s in enumerate(clusters) if class2 in s][0]
+        else:
+            set_scores_1 = np.array(
+                [set_score(friends, class1, s) for s in clusters])
+            set_scores_2 = np.array(
+                [set_score(friends, class2, s) for s in clusters])
+            opt_set = np.argmin(set_scores_1 + set_scores_2)
+        clusters[opt_set].add(class1)
+        clusters[opt_set].add(class2)
+        seen.add(class1)
+        seen.add(class2)
+        scores[class1, class2] = -1
+        scores[class2, class1] = -1
+    return clusters
+
+
+def greedy_clustering_overlapping(friends, N):
+    """
+        Returns a list of N sets, so that each set is the most friendly as
+        possible. Note that this allows for a class to be in two sets.
+    """
+    nb_classes = len(friends)
+    clusters = [set() for _ in xrange(N)]
+    seen = set()
+    scores = friends.copy()
+    while len(seen) < nb_classes:
+        best = np.argmax(scores)
+        class1, class2 = best = (best / nb_classes, best % nb_classes)
+        if class1 in seen:
+            opt_set = [i for i, s in enumerate(clusters) if class1 in s][0]
+        elif class2 in seen:
+            opt_set = [i for i, s in enumerate(clusters) if class2 in s][0]
+        else:
+            set_scores_1 = np.array(
+                [set_score(friends, class1, s) for s in clusters])
+            set_scores_2 = np.array(
+                [set_score(friends, class2, s) for s in clusters])
+            opt_set = np.argmin(set_scores_1 + set_scores_2)
+        clusters[opt_set].add(class1)
+        clusters[opt_set].add(class2)
+        seen.add(class1)
+        seen.add(class2)
+        scores[class1, class2] = -1
+        scores[class2, class1] = -1
+    return clusters
+
+
 if __name__ == '__main__':
     # model = load_cifar100_train32_test50()
     # data, par = load_data()
@@ -135,7 +220,18 @@ if __name__ == '__main__':
     # train_pred_classes = np.argmax(train_pred_probs, axis=1)
     test_pred_classes = np.argmax(test_pred_probs, axis=1)
     cm_categorical = confusion_matrix(test_targets, test_pred_classes)
-    cm_soft = soft_sum_cm(test_targets, test_pred_probs)
+    cm_categorical = clean_cm(cm_categorical)
+    # cm_soft = soft_sum_cm(test_targets, test_pred_probs)
     #cm_soft = soft_sum_pred_cm(test_targets, test_pred_probs)
-    plot_confusion_matrix(cm_soft, title=experiment)
+    friendliness = unfriendliness_matrix(cm_categorical)
+    #plot_confusion_matrix(friendliness)
+    clusters = greedy_clustering(friendliness, 5)
+
+    # plot_confusion_matrix(cm_soft, title=experiment)
     # plot_confusion_matrix(cm_categorical, title=experiment)
+
+
+    # To try when generating sets:
+    # - overlapping vs no-overlapping
+    # - greedy(+ friendliness) vs k-means vs spectral clustering
+    # - Try for all the different kind of cm (cat, soft, soft_pred, soft_n_pred)
