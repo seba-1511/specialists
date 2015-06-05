33f0938e0342700d3ba3d3054f2d6483cfa4d70d
seba-1511 <seba-1511@hotmail.com>
2015-06-05 00:06:32 -0700

    Implemented different confusion matrices and plotting.

--- code/6_gen_spec_data.py
+++ code/6_gen_spec_data.py
@@ -11,6 +11,7 @@ import numpy as np
 import cPickle as pk
 
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
 from neon.backends.par import NoPar
 from neon.datasets import CIFAR100
@@ -55,8 +56,71 @@ def load_data():
     return data, par
 
 
-def soft_cm(targets, preds):
-    return [[]]
+def clean_cm(cm):
+    for i in xrange(len(cm)):
+        cm[i, i] = 0
+    return cm
+
+
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(cm))
+    plt.xticks(tick_marks, xrange(len(cm)), rotation=45)
+    plt.yticks(tick_marks, xrange(len(cm)))
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+    plt.close()
+
+
+def soft_sum_cm(targets, preds):
+    """
+        Return a confusion matrix, containing the sum of all probabilities for
+        a given class.
+   """
+    N = max(targets)
+    nb_preds = len(preds)
+    placeholder = np.zeros(nb_preds)
+    cm = np.zeros((N, N))
+    for label in xrange(N):
+        entry = []
+        for i in xrange(N):
+            values = np.where(targets == label, preds[:, i], placeholder)
+            entry.append(sum(values))
+        cm[label] += entry
+    return clean_cm(cm)
+
+
+def soft_sum_pred_cm(targets, preds):
+    """
+        Returns a confusion matrix, where the the content of a cell is the sum
+        of prediction probabilities, only when this class was predicted.
+    """
+    N = max(targets)
+    nb_preds = len(preds)
+    placeholder = np.zeros(nb_preds)
+    cm = np.zeros((N, N))
+    for label in xrange(N):
+        entry = []
+        for i in xrange(N):
+            values = np.where(targets == label, preds[:, i], placeholder)
+            values = np.where(
+                label == np.argmax(preds, axis=1), values, placeholder)
+            entry.append(sum(values))
+        cm[label] += entry
+    return clean_cm(cm)
+
+
+def soft_sum_n_pred_cm(targets, preds, n=5):
+    """
+        Same as soft_sum_pred_cm, but we consider the case when the class is in
+        the top n predictions. (Might be usefull when a model is not so far to
+        predict the right class)
+    """
+    pass
 
 
 if __name__ == '__main__':
@@ -64,13 +128,14 @@ if __name__ == '__main__':
     # data, par = load_data()
     # targets = data.targets['test']
     # pred_probs = model.predict_proba(data.inputs['test'])
+
     experiment = '5_test50_train33_156epochs'
     train_pred_probs, test_pred_probs = load_inferences(name=experiment)
     train_targets, test_targets = load_targets(name=experiment)
     # train_pred_classes = np.argmax(train_pred_probs, axis=1)
     test_pred_classes = np.argmax(test_pred_probs, axis=1)
     cm_categorical = confusion_matrix(test_targets, test_pred_classes)
-    cm_soft = soft_cm(test_targets, test_pred_probs)
-
-    #import pdb
-    #pdb.set_trace()
+    cm_soft = soft_sum_cm(test_targets, test_pred_probs)
+    #cm_soft = soft_sum_pred_cm(test_targets, test_pred_probs)
+    plot_confusion_matrix(cm_soft, title=experiment)
+    # plot_confusion_matrix(cm_categorical, title=experiment)
