7f0ed39b1194d6498c1d785c77c8e4fc454d5a81
seba-1511 <seba-1511@hotmail.com>
2015-09-11 12:21:10 -0700

    Fixed train set mistake, added allconv net.

--- code/1_train_generalist.py
+++ code/1_train_generalist.py
@@ -5,6 +5,8 @@
 This file is an experiment that will train a specified generalist network.
 """
 
+import numpy as np
+
 from neon.backends import gen_backend
 from neon.data import DataIterator, load_cifar10
 from neon.optimizers import GradientDescentMomentum, Schedule, MultiOptimizer
@@ -14,32 +16,37 @@ from neon.transforms.cost import Misclassification
 from neon.callbacks.callbacks import Callbacks
 from neon.util.argparser import NeonArgparser
 
+from cifar_net import get_custom_vgg, get_allconv, get_dummy
+
 # parse the command line arguments
 parser = NeonArgparser(__doc__)
 args = parser.parse_args()
-
-from cifar_net import get_model
 
 
 if __name__ == '__main__':
     # hyperparameters
     batch_size = 128
     num_epochs = args.epochs
+    num_epochs = 74 if num_epochs == 10 else num_epochs
+    rng_seed = 1234
+    rng_seed = args.rng_seed
 
     # setup backend
     be = gen_backend(
         backend=args.backend,
         batch_size=batch_size,
-        rng_seed=args.rng_seed,
+        rng_seed=rng_seed,
         device_id=args.device_id,
         default_dtype=args.datatype,
     )
 
     (X_train, y_train), (X_test, y_test), nout = load_cifar10(path=args.data_dir)
-    model = get_model(nout=nout)
+    # That's only for the allconv net:
+    nout = 16
+    model = get_allconv(nout=nout)
     # TODO: Split train to get a validation set
-    train_set = DataIterator(X_test, y_train, nclass=nout)
-    test_set = DataIterator(X_test, y_test, nclass=nout)
+    train_set = DataIterator(X_train, y_train, nclass=nout, lshape=(3, 32, 32))
+    test_set = DataIterator(X_test, y_test, nclass=nout, lshape=(3, 32, 32))
 
     opt_schedule = Schedule(step_config=20, change=0.1)
     opt_gdm = GradientDescentMomentum(
@@ -53,19 +60,27 @@ if __name__ == '__main__':
         wdecay=0.0005,
         schedule=opt_schedule,
     )
+    opt_allconv = GradientDescentMomentum(
+        learning_rate=0.5,
+        schedule=opt_schedule,
+        momentum_coef=0.9,
+        wdecay=0.0001,
+    )
     opt = MultiOptimizer({
         'default': opt_gdmwd,
         'Bias': opt_gdm,
     })
 
-    cost = GeneralizedCost(costfunc=CrossEntropyMulti())
+    scaled_cost = GeneralizedCost(costfunc=CrossEntropyMulti(epsilon=0.0005, scale=1000))
+    general_cost = GeneralizedCost(costfunc=CrossEntropyMulti())
 
     callbacks = Callbacks(model, train_set, output_file=args.output_file,
-                      valid_set=test_set, valid_freq=args.validation_freq,
-                      progress_bar=args.progress_bar)
+                          valid_set=test_set, valid_freq=args.validation_freq,
+                          progress_bar=args.progress_bar)
 
-    model.fit(train_set, optimizer=opt, num_epochs=num_epochs, cost=cost, callbacks=callbacks)
+    model.fit(train_set, optimizer=opt_allconv, num_epochs=num_epochs, cost=general_cost, callbacks=callbacks)
 
+    print 'Train misclassification error: ', model.eval(train_set, metric=Misclassification())
     print 'Test misclassification error: ', model.eval(test_set, metric=Misclassification())
 
     # TODO: Export trained model to a given .pkl
--- code/cifar_net.py
+++ code/cifar_net.py
@@ -7,6 +7,7 @@ from neon.layers import (
     Pooling,
     Dropout,
     BatchNorm,
+    Activation,
 )
 from neon.models import Model
 from neon.transforms import (
@@ -14,10 +15,10 @@ from neon.transforms import (
     Rectlin,
     Softmax,
 )
-from neon.initializers import GlorotUniform
+from neon.initializers import GlorotUniform, Uniform, Constant
 
 
-def get_model(archive_path=None, nout=10):
+def get_custom_vgg(archive_path=None, nout=10):
     """
     This functions creates and returns the network that will be used for the
     generalists and the specialists.
@@ -27,31 +28,32 @@ def get_model(archive_path=None, nout=10):
     fc_init = GlorotUniform()
     last_init = GlorotUniform()
     layers = [
-        Conv((3, 3, 128), init=conv_init, activation=Rectlin(), pad=1),
+        Conv((3, 3, 128), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1),
         BatchNorm(),
-        Conv((3, 3, 128), init=conv_init, activation=Rectlin(), pad=1),
-        BatchNorm(),
-        Pooling((2, 2), strides=2, op='max'),
-
-        Conv((3, 3, 256), init=conv_init, activation=Rectlin(), pad=1),
-        BatchNorm(),
-        Conv((3, 3, 256), init=conv_init, activation=Rectlin(), pad=1),
+        Conv((3, 3, 128), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1),
         BatchNorm(),
         Pooling((2, 2), strides=2, op='max'),
 
-        Conv((3, 3, 512), init=conv_init, activation=Rectlin(), pad=1),
+        Conv((3, 3, 256), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1),
         BatchNorm(),
-        Conv((3, 3, 512), init=conv_init, activation=Rectlin(), pad=1),
+        Conv((3, 3, 256), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1),
         BatchNorm(),
         Pooling((2, 2), strides=2, op='max'),
 
-        Affine(4096, init=fc_init, activation=Rectlin()),
+        Conv((3, 3, 512), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1),
+        BatchNorm(),
+        Conv((3, 3, 512), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1),
+        BatchNorm(),
+        Pooling((2, 2), strides=2, op='max'),
+
+        Affine(nout=4096, init=fc_init, bias=Constant(1), activation=Rectlin()),
         BatchNorm(),
         Dropout(0.5),
-        Affine(4096, init=fc_init, activation=Rectlin()),
+        Affine(nout=4096, init=fc_init, bias=Constant(1), activation=Rectlin()),
         BatchNorm(),
         Dropout(0.5),
-        Affine(nout, init=last_init, activation=Softmax()),
+        Affine(nout=nout, init=last_init, bias=Constant(-7), activation=Rectlin()),
+        Activation(Softmax()),
     ]
     # Instanciate model
     if archive_path != None:
@@ -59,3 +61,31 @@ def get_model(archive_path=None, nout=10):
     # Set model weights
 
     return Model(layers=layers)
+
+def get_allconv(archive_path=None, nout=10):
+    init_uni = GlorotUniform()
+    layers = []
+    layers.append(Dropout(keep=.8))
+    layers.append(Conv((3, 3, 96), init=init_uni, batch_norm=True, activation=Rectlin()))
+    layers.append(Conv((3, 3, 96), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1))
+    layers.append(Conv((3, 3, 96), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1, strides=2))
+    layers.append(Dropout(keep=.5))
+    layers.append(Conv((3, 3, 192), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1))
+    layers.append(Conv((3, 3, 192), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1))
+    layers.append(Conv((3, 3, 192), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1, strides=2))
+    layers.append(Dropout(keep=.5))
+    layers.append(Conv((3, 3, 192), init=init_uni, batch_norm=True, activation=Rectlin()))
+    layers.append(Conv((1 ,1, 192), init=init_uni, batch_norm=True, activation=Rectlin()))
+    layers.append(Conv((1,1,16), init=init_uni, activation=Rectlin()))
+    layers.append(Pooling(6, op="avg"))
+    layers.append(Activation(Softmax()))
+    return Model(layers=layers)
+
+def get_dummy(archive_path=None, nout=10):
+    return Model(
+        layers=[
+            Affine(100),
+            Affine(100),
+        ]
+    )
+
