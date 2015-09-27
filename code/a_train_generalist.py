b6242034cc4bcd84a698b1df4aab7e2b96e599bd
seba-1511 <seba-1511@hotmail.com>
2015-09-27 14:19:03 -0700

    Added cifar100.

--- code/a_train_generalist.py
+++ code/a_train_generalist.py
@@ -14,13 +14,17 @@ from neon.callbacks.callbacks import Callbacks
 from neon.util.argparser import NeonArgparser
 from neon.util.persist import save_obj
 
+from keras.datasets import cifar100
+
 from cifar_net import get_custom_vgg, get_allconv, get_dummy
 
 # parse the command line arguments
 parser = NeonArgparser(__doc__)
 args = parser.parse_args()
 
-EXPERIMENT_DIR = 'experiments/cifar10/'
+DATASET_NAME = 'cifar100'
+EXPERIMENT_DIR = 'experiments/' + DATASET_NAME + '/'
+VALIDATION = False
 
 
 def split_train_set(X_train, y_train):
@@ -42,26 +46,39 @@ if __name__ == '__main__':
         default_dtype=args.datatype,
     )
 
-    (X_train, y_train), (X_test, y_test), nout = load_cifar10(path=args.data_dir)
-    (X_train, y_train), (X_valid, y_valid) = split_train_set(X_train, y_train)
-    # That's only for the allconv net:
-    # nout = 16
-    model, opt, cost = get_allconv(nout=nout)
+    if DATASET_NAME == 'cifar10':
+        (X_train, y_train), (X_test, y_test), nout = load_cifar10(path=args.data_dir)
+        nout = 16
+    elif DATASET_NAME == 'cifar100':
+        (X_train, y_train), (X_test, y_test) = cifar100.load_data(label_mode='fine')
+        X_train = X_train.reshape(50000, 3072)
+        X_test = X_test.reshape(10000, 3072)
+        nout = 128
 
-    # TODO: Split train to get a validation set
+    if VALIDATION:
+        (X_train, y_train), (X_valid, y_valid) = split_train_set(X_train, y_train)
+    model, opt, cost = get_custom_vgg(nout=nout)
+
     train_set = DataIterator(X_train, y_train, nclass=nout, lshape=(3, 32, 32))
-    valid_set = DataIterator(X_valid, y_valid, nclass=nout, lshape=(3, 32, 32))
     test_set = DataIterator(X_test, y_test, nclass=nout, lshape=(3, 32, 32))
 
     callbacks = Callbacks(model, train_set, output_file=args.output_file,
-                          valid_set=valid_set, valid_freq=args.validation_freq,
+                          valid_set=test_set, valid_freq=args.validation_freq,
                           progress_bar=args.progress_bar)
+    if VALIDATION:
+        valid_set = DataIterator(X_valid, y_valid, nclass=nout, lshape=(3, 32, 32))
+        callbacks = Callbacks(model, train_set, output_file=args.output_file,
+                            valid_set=valid_set, valid_freq=args.validation_freq,
+                            progress_bar=args.progress_bar)
 
     model.fit(train_set, optimizer=opt, num_epochs=num_epochs, cost=cost, callbacks=callbacks)
 
+    print 'Validation: ', VALIDATION
     print 'Train misclassification error: ', model.eval(train_set, metric=Misclassification())
-    print 'Valid misclassification error: ', model.eval(valid_set, metric=Misclassification())
+    if VALIDATION:
+        print 'Valid misclassification error: ', model.eval(valid_set, metric=Misclassification())
     print 'Test misclassification error: ', model.eval(test_set, metric=Misclassification())
 
-    save_obj(model.serialize(), EXPERIMENT_DIR + args.save_path)
+    if args.save_path is not None:
+        save_obj(model.serialize(), EXPERIMENT_DIR + args.save_path)
 
