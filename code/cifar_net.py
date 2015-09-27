cff66bef50f0aa50b9e6147a2f30a25a1d56884b
seba-1511 <seba-1511@hotmail.com>
2015-09-27 14:19:40 -0700

    Added augmentation script.

--- code/cifar_net.py
+++ code/cifar_net.py
@@ -26,26 +26,26 @@ def get_custom_vgg(archive_path=None, nout=10):
     This functions creates and returns the network that will be used for the
     generalists and the specialists.
     """
-    # TODO: Check that init works properly.
     conv_init = GlorotUniform()
     fc_init = GlorotUniform()
     last_init = GlorotUniform()
     layers = [
+        # Initially Convs are: 128, 256, 512 Affines: 4096, 4096
+        Conv((3, 3, 32), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
+        Conv((3, 3, 32), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
+        Pooling((2, 2), strides=2, op='max'),
+
+        Conv((3, 3, 64), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
+        Conv((3, 3, 64), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
+        Pooling((2, 2), strides=2, op='max'),
+
         Conv((3, 3, 128), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
         Conv((3, 3, 128), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
         Pooling((2, 2), strides=2, op='max'),
 
-        Conv((3, 3, 256), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
-        Conv((3, 3, 256), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
-        Pooling((2, 2), strides=2, op='max'),
-
-        Conv((3, 3, 512), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
-        Conv((3, 3, 512), init=conv_init, bias=Constant(0), activation=Rectlin(), pad=1, batch_norm=True),
-        Pooling((2, 2), strides=2, op='max'),
-
-        Affine(nout=4096, init=fc_init, bias=Constant(1), activation=Rectlin(), batch_norm=True),
+        Affine(nout=1024, init=fc_init, bias=Constant(1), activation=Rectlin(), batch_norm=True),
         Dropout(0.5),
-        Affine(nout=4096, init=fc_init, bias=Constant(1), activation=Rectlin(), batch_norm=True),
+        Affine(nout=1024, init=fc_init, bias=Constant(1), activation=Rectlin(), batch_norm=True),
         Dropout(0.5),
         Affine(nout=nout, init=last_init, bias=Constant(-7), activation=Softmax()),
         # Activation(Softmax()),
@@ -55,6 +55,7 @@ def get_custom_vgg(archive_path=None, nout=10):
         # Set model weights
         model.load_weights(archive_path)
     opt_schedule = Schedule(step_config=20, change=0.1)
+    # opt_schedule = Schedule(step_config=[20, 40, 60, 80, 100, 120, 140, 160], change=[0.1, 0.05, 0.025, 0.01, 0.005, 0.0001, 0.00005, 0.00001])
     opt_gdm = GradientDescentMomentum(
         learning_rate=0.0002,
         momentum_coef=0.90,
@@ -70,34 +71,37 @@ def get_custom_vgg(archive_path=None, nout=10):
         'default': opt_gdmwd,
         'Bias': opt_gdm,
     })
+    # cost = GeneralizedCost(costfunc=CrossEntropyMulti())
     cost = GeneralizedCost(costfunc=CrossEntropyMulti(epsilon=0.0005, scale=1000))
     return model, opt, cost
 
 def get_allconv(archive_path=None, nout=10):
     init_uni = GlorotUniform()
     layers = []
-    layers.append(Dropout(keep=.8))
+    layers.append(Dropout(keep=0.8))
     layers.append(Conv((3, 3, 96), init=init_uni, batch_norm=True, activation=Rectlin()))
     layers.append(Conv((3, 3, 96), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1))
     layers.append(Conv((3, 3, 96), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1, strides=2))
-    layers.append(Dropout(keep=.5))
+    layers.append(Dropout(keep=0.5))
     layers.append(Conv((3, 3, 192), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1))
     layers.append(Conv((3, 3, 192), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1))
     layers.append(Conv((3, 3, 192), init=init_uni, batch_norm=True, activation=Rectlin(), pad=1, strides=2))
-    layers.append(Dropout(keep=.5))
+    layers.append(Dropout(keep=0.5))
     layers.append(Conv((3, 3, 192), init=init_uni, batch_norm=True, activation=Rectlin()))
-    layers.append(Conv((1 ,1, 192), init=init_uni, batch_norm=True, activation=Rectlin()))
-    layers.append(Conv((1,1,16), init=init_uni, activation=Rectlin()))
+    layers.append(Conv((1, 1, 192), init=init_uni, batch_norm=True, activation=Rectlin()))
+    layers.append(Conv((1, 1, 16), init=init_uni, activation=Rectlin()))
     layers.append(Pooling(6, op="avg"))
     layers.append(Activation(Softmax()))
     model = Model(layers=layers)
     if archive_path is not None:
         model.load_weights(archive_path)
+    # opt_schedule = Schedule(step_config=[20, 40, 60, 80, 100, 120, 140, 160], change=[0.1, 0.05, 0.025, 0.01, 0.005, 0.0001, 0.00005, 0.00001])
     opt_schedule = Schedule(step_config=20, change=0.1)
     opt = GradientDescentMomentum(
         learning_rate=0.5,
         schedule=opt_schedule,
         momentum_coef=0.9,
+        # wdecay=0.00077,
         wdecay=0.00075,
     )
     cost = GeneralizedCost(costfunc=CrossEntropyMulti())
---
+++ code/d_train_generalist_augmented.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file is an experiment that will train a specified generalist network.
+"""
+
+import numpy as np
+
+from neon.backends import gen_backend
+from neon.data import DataIterator, load_cifar10
+from neon.transforms.cost import Misclassification
+from neon.callbacks.callbacks import Callbacks
+from neon.util.argparser import NeonArgparser
+from neon.util.persist import save_obj
+
+from keras.datasets import cifar100
+from keras.preprocessing.image import ImageDataGenerator
+
+from cifar_net import get_custom_vgg, get_allconv, get_dummy
+
+# parse the command line arguments
+parser = NeonArgparser(__doc__)
+args = parser.parse_args()
+
+DATASET_NAME = 'cifar100'
+EXPERIMENT_DIR = 'experiments/' + DATASET_NAME + '/'
+VALIDATION = False
+
+
+def split_train_set(X_train, y_train):
+    return (X_train[:-5000], y_train[:-5000]), (X_train[-5000:], y_train[-5000:])
+
+if __name__ == '__main__':
+    # hyperparameters
+    batch_size = 64
+    num_epochs = args.epochs
+    num_epochs = 74 if num_epochs == 10 else num_epochs
+    rng_seed = 1234
+
+    # setup backend
+    be = gen_backend(
+        backend=args.backend,
+        batch_size=batch_size,
+        rng_seed=rng_seed,
+        device_id=args.device_id,
+        default_dtype=args.datatype,
+    )
+
+    # Setup Data Augmentation:
+    print 'Setting up data augmentation...'
+    augmenter = ImageDataGenerator(
+        featurewise_center=True,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=True,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=True,  # apply ZCA whitening
+        rotation_range=20,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+    )
+
+    # Setup Model and Datasets
+    print 'Loading data...'
+    if DATASET_NAME == 'cifar10':
+        (X_train, y_train), (X_test, y_test), nout = load_cifar10(path=args.data_dir)
+        nout = 16
+    elif DATASET_NAME == 'cifar100':
+        (X_train, y_train), (X_test, y_test) = cifar100.load_data(label_mode='fine')
+        X_train_keras = X_train.copy()
+        X_train = X_train.reshape(50000, 3072)
+        X_test = X_test.reshape(10000, 3072)
+        nout = 128
+
+    if VALIDATION:
+        (X_train, y_train), (X_valid, y_valid) = split_train_set(X_train, y_train)
+
+    model, opt, cost = get_custom_vgg(nout=nout)
+
+    # train_set = DataIterator(X_train, y_train, nclass=nout, lshape=(3, 32, 32))
+    test_set = DataIterator(X_test, y_test, nclass=nout, lshape=(3, 32, 32))
+
+    if VALIDATION:
+        valid_set = DataIterator(X_valid, y_valid, nclass=nout, lshape=(3, 32, 32))
+        callbacks = Callbacks(model, train_set, output_file=args.output_file,
+                            valid_set=valid_set, valid_freq=args.validation_freq,
+                            progress_bar=args.progress_bar)
+
+    # Training Procedure
+    print 'Begin training...'
+    print 'Calibrating augmenter...'
+    augmenter.fit(X_train_keras)
+    print 'Setting up model...'
+    model.cost = cost
+    model.set_shortcut()
+    model.optimizer = opt
+    model.total_cost = be.empty((1, 1))
+    while model.epoch_index < num_epochs and not model.finished:
+        # Data Augmentation
+        print 'Augmenting Data...'
+        X_train_augmented = np.empty(X_train_keras.shape)
+        y_train_augmented = np.emtpy(y_train.shape)
+        for X, y in augmenter.flow(X_train_keras, y_train):
+            X_train_augmented = np.vstack((X_train_augmented, X))
+            y_train_augmented = np.vstack((y_train_augmented, y))
+        X_train_augmented = X_train_augmented.reshape(X_train_augmented.shape[0], 3072)
+
+        if not VALIDATION:
+            callbacks = Callbacks(model, train_set, output_file=args.output_file,
+                            valid_set=test_set, valid_freq=args.validation_freq,
+                            progress_bar=args.progress_bar)
+
+        train_set = DataIterator(X_train_augmented, y_train_augmented, nclass=nout, lshape=(3, 32, 32))
+        model._epoch_fit(train_set, callbacks)
+
+        callbacks.on_epoch_end(model.epoch_index)
+        model.epoch_index += 1
+    callbacks.on_train_end()
+
+
+    # Print Results
+    print 'Validation: ', VALIDATION
+    print 'Train misclassification error: ', model.eval(train_set, metric=Misclassification())
+    if VALIDATION:
+        print 'Valid misclassification error: ', model.eval(valid_set, metric=Misclassification())
+    print 'Test misclassification error: ', model.eval(test_set, metric=Misclassification())
+
+    if args.save_path is not None:
+        save_obj(model.serialize(), EXPERIMENT_DIR + 'augmented_' + args.save_path)
+
